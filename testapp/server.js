8bdcc074e74e431c062d92be7b93393def9e0a1e
angellabn <angellabn77@gmail.com>
2026-06-28 20:05:25 +0530

    jshw

--- testapp/server.js
+++ testapp/server.js
@@ -35,7 +35,7 @@ app.post("/addUser", async (req, res) => {
     await client.connect();
     console.log("Connected successfully to server");
 
-    const db = client.db("appnacollege-db");
+    const db = client.db("usern-db");
     const data = await db.collection("users").insertOne(userObj);
 
     console.log(data);
